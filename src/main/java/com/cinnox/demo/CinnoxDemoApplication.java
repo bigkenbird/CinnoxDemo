d7539f96476aca74693b77780d95fdeae1e258b6
bigkenbird <k918231524@gmail.com>
2023-03-11 20:16:43 +0800

    first commit

---
+++ src/main/java/com/cinnox/demo/CinnoxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cinnox.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinnoxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinnoxDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinnox/demo/CinnoxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinnox.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinnoxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
